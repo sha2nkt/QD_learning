efd47ea093ebd1c95d1a835f80219ba25286c2b6
Shashank <shashant@andrew.cmu.edu>
2018-04-25 19:11:30 -0400

    Action Bug fixed autoencoder model

--- sample_effient_net/autoencoder_model.py
+++ sample_effient_net/autoencoder_model.py
@@ -10,48 +10,57 @@ class PredictorNet(nn.Module):
         # TODO: Define model
         self.forward_conv = nn.Sequential(
             # Input: 256x256
-            nn.Conv2d(12, 64,kernel_size=4, stride=2),
+            nn.Conv2d(12, 64,kernel_size=8, stride=2, padding=1),
             nn.ReLU(True),
-            nn.Conv2d(64, 128,kernel_size=3,stride=2),
+            nn.Conv2d(64, 128,kernel_size=6,stride=2, padding=1),
             nn.ReLU(True),
-            nn.Conv2d(128, 128, kernel_size=3, stride=2),
+            nn.Conv2d(128, 128, kernel_size=6, stride=2, padding=1),
             nn.ReLU(True),
-            # nn.Conv2d(128, 128, kernel_size=3, stride=2),
-            # nn.ReLU(True),
+            nn.Conv2d(128, 128, kernel_size=6, stride=2, padding=1),
+            nn.ReLU(True),
         )
         self.encoder = nn.Sequential(
-            nn.Linear(128 * 7 * 7, 1024),
+            nn.Linear(128 * 11 * 8, 1024),
             nn.ReLU(True),
             nn.Linear(1024, 2048)
         )
         self.forward_actions = nn.Sequential(
-            nn.Linear(5, 1024),
+            nn.Linear(6, 1024),
             nn.ReLU(True),
             nn.Linear(1024, 2048)
+        )
+
+        self.reward = nn.Sequential(
+            nn.Linear(2048, 512),
+            nn.ReLU(True),
+            nn.Linear(512,128),
+            nn.ReLU(True),
+            nn.Linear(128,3),
         )
         self.decoder = nn.Sequential(
             nn.Linear(2048, 1024),
             nn.ReLU(True),
-            nn.Linear(1024, 128 * 7 * 7) #12 for 256
+            nn.Linear(1024, 128 * 11 * 8) #12 for 256
         )
         self.forward_deconv = nn.Sequential(
-            # nn.ConvTranspose2d(128, 128, kernel_size=3, stride=2),
-            # nn.ReLU(True),
-            nn.ConvTranspose2d(128, 128, kernel_size=3, stride=2),
+            nn.ConvTranspose2d(128, 128, kernel_size=6, stride=2, padding=1),
             nn.ReLU(True),
-            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2),
+            nn.ConvTranspose2d(128, 128, kernel_size=6, stride=2, padding=1),
             nn.ReLU(True),
-            nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2),
+            nn.ConvTranspose2d(128, 64, kernel_size=6, stride=2, padding=1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 3, kernel_size=8, stride=2, padding=(0,1)),
         )
 
     def forward(self, x, a):
         # TODO: Define forward pass
         x = self.forward_conv(x)
-        x = x.view(-1, 128 * 7 * 7)
+        x = x.view(-1, 128 * 11 * 8)
         x = self.encoder(x)
         a = self.forward_actions(a)
         x = x * a
+        r = self.reward(x)
         x = self.decoder(x)
-        x = x.view(-1, 128, 7, 7)
+        x = x.view(-1, 128, 11, 8)
         x = self.forward_deconv(x)
-        return x
+        return x, r
--- sample_effient_net/autoencoder_trainer.py
+++ sample_effient_net/autoencoder_trainer.py
@@ -1,7 +1,7 @@
 import pickle
 import torch
 from autoencoder_model import *
-from gridworldv2 import gameEnv
+from gridworld import gameEnv
 
 import torchvision.transforms as transforms
 import logger
@@ -9,10 +9,11 @@ import pdb
 import random
 from collections import deque
 import shutil
+import gym
 
 class Memory(object):
 
-    def __init__(self, memory_size=100, burn_in=10):
+    def __init__(self, memory_size=1000, burn_in=10):
 
         self.memory = deque(maxlen=memory_size)
 
@@ -39,97 +40,130 @@ class Memory(object):
 class Agent():
 
     def __init__(self):
-        self.env_size = 64
-        self.env = gameEnv(partial=False, size=self.env_size)
-        self.max_epLength = 100
-        self.num_episodes = 200
-        self.nS = 81
+        # self.env_size = 64
+        # self.env = gameEnv(partial=False, size=self.env_size)
+        self.env = gym.make('Pong-v0')
+        # self.max_epLength = 100
+        self.num_episodes = 10000
         self.batch_size = 32
         self.trace_length = 5
         self.lr = 1e-4
+        self.num_train_iters = 10
         self.model = PredictorNet()
         self.model.cuda()
-        self.criterion = nn.MultiLabelSoftMarginLoss().cuda()
+        # self.criterion = nn.MultiLabelSoftMarginLoss().cuda()
+        self.criterion = nn.MSELoss().cuda()
+        self.r_criterion = nn.CrossEntropyLoss().cuda()
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
-        self.tb_folder = './pred_autoenc_0/'
+        self.tb_folder = './pong_autoenc_0/'
         shutil.rmtree(self.tb_folder, ignore_errors=True)
         self.tb = logger.Logger(self.tb_folder, name='freeloc')
-        self.model_file = './pred_auto_model_0'
+        self.model_file = './pong_auto_model_0'
         self.global_step = 0
         self.memory = Memory()
+
+    def generate_training_data(self):
+        train_data = self.memory.sample_batch(self.batch_size, self.trace_length)
+        batch_s = []
+        batch_a = []
+        batch_r = []
+        batch_ns = []
+        for ep_data in train_data:
+            states = []
+            actions = []
+            rewards = []
+            for frames_data in ep_data:
+                states.append(frames_data[0])
+                actions.append(frames_data[1])
+                rewards.append(frames_data[2])
+
+            states = np.vstack(states)
+            actions = np.vstack(actions)
+            rewards = np.vstack(rewards)
+            input_states = states[:12]
+            input_actions = actions[:12]
+            input_rewards = rewards[:12]
+            target_states = states[12:]
+            batch_s.append(input_states)
+            batch_a.append(input_actions)
+            batch_r.append(input_rewards)
+            batch_ns.append(target_states)
+        batch_s = np.array(batch_s)
+        batch_a = np.array(batch_a)
+        batch_a = batch_a[:, -2, :]  # potential source of error
+        batch_r = np.array(batch_r)
+        batch_r = batch_r[:, -2, :]
+        batch_ns = np.array(batch_ns)
+
+        batch_s = torch.from_numpy(batch_s).type(torch.FloatTensor).cuda(async=True)
+        batch_a = torch.from_numpy(batch_a).type(torch.FloatTensor).cuda(async=True)
+        batch_r = torch.from_numpy(batch_r).type(torch.LongTensor).contiguous().cuda(async=True)
+        batch_ns = torch.from_numpy(batch_ns).type(torch.FloatTensor).cuda(async=True)
+        batch_s = torch.autograd.Variable(batch_s)
+        batch_a = torch.autograd.Variable(batch_a)
+        batch_r = torch.autograd.Variable(batch_r)
+        batch_ns = torch.autograd.Variable(batch_ns)
+        return batch_s, batch_a, batch_r, batch_ns
 
     def train(self):
         self.model.train()
         sigmoid = nn.Sigmoid()
 
         for n in range(0, self.num_episodes):
-            print('Episode:', n)
             ep_state_list = []
             ep_action_list = []
+            ep_reward_list = []
             ep_nextState_list = []
             s = self.env.reset()
             s = s.transpose((2,0,1))
-            for j in range(0, self.max_epLength):
+            done = False
+            while not done:
                 ep_state_list.append(s)
-                a = np.random.randint(0, 5)
-                ep_action_list.append(a)
-                ns, r, d = self.env.step(a)
+                a = np.random.randint(0, 6)
+                a_oh = np.zeros((1,6))
+                a_oh[0, a] = 1
+                ep_action_list.append(a_oh)
+                ns, r, done, _ = self.env.step(a)
+                # r_oh = np.zeros((1,3))
+                if r==-1:
+                    r=0
+                if r==0:
+                    r=1
+                if r==1:
+                    r=2
+                ep_reward_list.append(r)
                 ns = ns.transpose((2,0,1))
                 ep_nextState_list.append(ns)
                 s = ns
             ep_state_arr = np.array(ep_state_list)
             ep_action_arr = np.array(ep_action_list)
+            ep_reward_arr = np.array(ep_reward_list)
             ep_nextState_arr = np.array(ep_nextState_list)
-            episode_buffer = zip(ep_state_arr, ep_action_arr, ep_nextState_arr)
+            episode_buffer = zip(ep_state_arr, ep_action_arr, ep_reward_arr, ep_nextState_arr)
             self.memory.append(episode_buffer)
 
             if n>=self.batch_size:
                 # This is a list of batch_size lists each having trace_len tuples
-                train_data = self.memory.sample_batch(self.batch_size, self.trace_length)
-                batch_s = []
-                batch_a = []
-                batch_ns = []
-                for ep_data in train_data:
-                    states = []
-                    actions = []
-                    for frames_data in ep_data:
-                        states.append(frames_data[0])
-                        actions.append(frames_data[1])
+                for train_iters in range(self.num_train_iters):
+                    batch_s, batch_a, batch_r, batch_ns = self.generate_training_data()
+                    self.optimizer.zero_grad()
+                    preds, r = self.model.forward(batch_s, batch_a)
+                    loss = self.criterion(preds, batch_ns)
+                    r_loss = self.r_criterion(r, batch_r.view(-1,))
+                    net_loss = loss + r_loss
+                    net_loss.backward()
 
+                    self.optimizer.step()
+                    # preds = sigmoid(preds)
+                    # preds = preds >= 0.2
+                    # preds = preds.data.cpu().numpy()
+                    # batch_ns = batch_ns.data.cpu().numpy()
+                    # accuracy = np.sum(preds == batch_ns)*1.0 / (self.batch_size * 3 * self.env_size * self.env_size)
+                    # self.tb.scalar_summary('train/acc', accuracy, self.global_step)
+                    self.tb.scalar_summary('train/state_loss', loss, self.global_step)
+                    self.tb.scalar_summary('train/reward_loss', r_loss, self.global_step)
 
-                    states = np.vstack(states)
-                    actions = np.vstack(actions).reshape(-1,)
-                    input_states = states[:12]
-                    input_actions = actions[:12]
-                    target_states = states[12:]
-                    batch_s.append(input_states)
-                    batch_a.append(input_actions)
-                    batch_ns.append(target_states)
-                batch_s = np.array(batch_s)
-                batch_a = np.array(batch_a)
-                batch_ns = np.array(batch_ns)
-
-                batch_s = torch.from_numpy(batch_s).type(torch.FloatTensor).cuda(async=True)
-                batch_a = torch.from_numpy(batch_a).type(torch.FloatTensor).cuda(async=True)
-                batch_ns = torch.from_numpy(batch_ns).type(torch.FloatTensor).cuda(async=True)
-                batch_s = torch.autograd.Variable(batch_s)
-                batch_a = torch.autograd.Variable(batch_a)
-                batch_ns = torch.autograd.Variable(batch_ns)
-
-                self.optimizer.zero_grad()
-                preds = self.model.forward(batch_s, batch_a)
-                loss = self.criterion(preds, batch_ns)
-                loss.backward()
-
-                self.optimizer.step()
-                preds = sigmoid(preds)
-                preds = preds >= 0.2
-                preds = preds.data.cpu().numpy()
-                batch_ns = batch_ns.data.cpu().numpy()
-                accuracy = np.sum(preds == batch_ns)*1.0 / (self.batch_size * 3 * self.env_size * self.env_size)
-                self.tb.scalar_summary('train/acc', accuracy, self.global_step)
-                self.tb.scalar_summary('train/loss', loss, self.global_step)
-                self.global_step += 1
+                    self.global_step += 1
 
         self.save_model_weights()
 
@@ -228,57 +262,79 @@ class Agent():
         for n in range(0, self.num_episodes):
             ep_state_list = []
             ep_action_list = []
+            ep_reward_list = []
             ep_nextState_list = []
             s = self.env.reset()
-            s = s.transpose((2,0,1))
+            s = s.transpose((2, 0, 1))
             for j in range(0, self.max_epLength):
                 ep_state_list.append(s)
-                a = np.random.randint(0, 5)
-                ep_action_list.append(a)
-                ns, r, d = self.env.step(a)
-                ns = ns.transpose((2,0,1))
+                a = np.random.randint(0, 6)
+                a_oh = np.zeros((1, 6))
+                a_oh[0, a] = 1
+                ep_action_list.append(a_oh)
+                ns, r, d, _ = self.env.step(a)
+                # r_oh = np.zeros((1,3))
+                if r == -1:
+                    r = 0
+                if r == 0:
+                    r = 1
+                if r == 1:
+                    r = 2
+                ep_reward_list.append(r)
+                ns = ns.transpose((2, 0, 1))
                 ep_nextState_list.append(ns)
                 s = ns
             ep_state_arr = np.array(ep_state_list)
             ep_action_arr = np.array(ep_action_list)
+            ep_reward_arr = np.array(ep_reward_list)
             ep_nextState_arr = np.array(ep_nextState_list)
-            episode_buffer = zip(ep_state_arr, ep_action_arr, ep_nextState_arr)
+            episode_buffer = zip(ep_state_arr, ep_action_arr, ep_reward_arr, ep_nextState_arr)
             self.memory.append(episode_buffer)
 
-            if n>=self.batch_size:
+            if n >= self.batch_size:
                 # This is a list of batch_size lists each having trace_len tuples
                 train_data = self.memory.sample_batch(self.batch_size, self.trace_length)
                 batch_s = []
                 batch_a = []
+                batch_r = []
                 batch_ns = []
                 for ep_data in train_data:
                     states = []
                     actions = []
+                    rewards = []
                     for frames_data in ep_data:
                         states.append(frames_data[0])
                         actions.append(frames_data[1])
-
+                        rewards.append(frames_data[2])
 
                     states = np.vstack(states)
-                    actions = np.vstack(actions).reshape(-1,)
+                    actions = np.vstack(actions)
+                    rewards = np.vstack(rewards)
                     input_states = states[:12]
                     input_actions = actions[:12]
+                    input_rewards = rewards[:12]
                     target_states = states[12:]
                     batch_s.append(input_states)
                     batch_a.append(input_actions)
+                    batch_r.append(input_rewards)
                     batch_ns.append(target_states)
                 batch_s = np.array(batch_s)
                 batch_a = np.array(batch_a)
+                batch_a = batch_a[:, -1, :]
+                batch_r = np.array(batch_r)
+                batch_r = batch_r[:, -1, :]
                 batch_ns = np.array(batch_ns)
 
                 batch_s = torch.from_numpy(batch_s).type(torch.FloatTensor).cuda(async=True)
                 batch_a = torch.from_numpy(batch_a).type(torch.FloatTensor).cuda(async=True)
+                batch_r = torch.from_numpy(batch_r).type(torch.LongTensor).contiguous().cuda(async=True)
                 batch_ns = torch.from_numpy(batch_ns).type(torch.FloatTensor).cuda(async=True)
                 batch_s = torch.autograd.Variable(batch_s)
                 batch_a = torch.autograd.Variable(batch_a)
+                batch_r = torch.autograd.Variable(batch_r)
                 batch_ns = torch.autograd.Variable(batch_ns)
 
-                preds = self.model.forward(batch_s, batch_a)
+                preds, r = self.model.forward(batch_s, batch_a)
 
                 preds = sigmoid(preds)
                 preds = preds >= 0.2
@@ -286,8 +342,7 @@ class Agent():
                 batch_ns = batch_ns.data.cpu().numpy()
 
                 accuracy = np.sum(preds == batch_ns)*1.0 / (self.batch_size * 3 * self.env_size * self.env_size)
-                # print(nextState.astype(np.int).reshape(9,9))
-                sum_acc += accuracy
+                sum_acc += acc
                 count_acc += 1
         print(sum_acc / count_acc)
 
@@ -332,7 +387,7 @@ def main():
 
     agent = Agent()
     # agent.train()
-    agent.test()
+    agent.train()
 
 
 if __name__ == '__main__':
